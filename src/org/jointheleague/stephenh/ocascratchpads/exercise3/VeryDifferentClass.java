51f22b7e20575afd0c75904151bb0c91b5afc923
BigRockerEnding <bigrockerending@live.com>
2017-01-15 17:29:23 -0800

    Exercise 3

---
+++ src/org/jointheleague/stephenh/ocascratchpads/exercise3/VeryDifferentClass.java
@@ -0,0 +1,7 @@
+package org.jointheleague.stephenh.ocascratchpads.exercise3;
+
+public class VeryDifferentClass {
+	public static void main(String[] args) {
+		System.out.println("Hello " + args[0]);
+	}
+}
